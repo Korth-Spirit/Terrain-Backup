7a95b9b2c14de91fe49b64663012c18fae32d1f6
Johnny Irvin <irvinjohnathan@gmail.com>
2022-05-03 12:25:05 -0400

    Remove unused import

--- backup/utilities.py
+++ backup/utilities.py
@@ -19,7 +19,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import json
-from typing import Iterable, List, Union
+from typing import Iterable, Union
 
 from korth_spirit.data import TerrainNodeData
 
